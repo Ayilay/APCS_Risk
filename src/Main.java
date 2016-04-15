78be2bfb4e705a37e0f8409d78e79b62c05e1eb0
Relic56 <ethan.y.yao@gmail.com>
2016-04-14 22:56:54 -0700

    Refined Main, fixed bugs
    It's ugly but it works XD

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,30 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main 
+public class Main
 {
 	private Player[] players;
 	private int numPlayers;
-	
+
 	// for cmd line inputs, will be replaced by GUI input
 	private BufferedReader br;
-	
+
 	public static void main(String[] args) throws IOException
 	{
 		Main riskGame = new Main();
 		riskGame.play();
 	}
-	
+
 	private void play() throws IOException
 	{
-		//TODO: implement GUI
+		// TODO: implement GUI
 		init();
-		
-		Die_Roll die;
-		
 
-		while(true)
+		Die_Roll die;
+
+		while (true)
 		{
-			for(Player p : players)
+			for (Player p : players)
 			{
 				// CMDLine stuff, will be replaced by GUI Output
 				String playerName = p.getName();
@@ -42,49 +41,83 @@ public class Main
 
 				// Phase 1: Deploy Reinforcements
 				deployReinforcements(p);
-				
+
 				// Phase 2: Attack a territory
 				attackOther(p);
-				
+
 			}
 		}
 	}
-	//right this is ugly but it's for testing purposes XD
+
+	// right this is ugly but it's for testing purposes XD
 	public void attackOther(Player p) throws IOException
 	{
+		String territoryFrom = "", territoryTo = "";
+		int numArmies = 0;
+
 		System.out.println("Choose a territory to attack");
+		boolean valid = false;
 		System.out.println(getAvailableNeighbors(p));
-		String territoryTo = br.readLine();
-		Territory t = TerritoryMap.get(territoryTo);
-		Set<String> temp = new HashSet<String>();
-		for(String s : t.getAdjacentTerritories())
+
+		while (!valid)
 		{
-			if(p.ownsTerritory(s))
+			territoryTo = br.readLine();
+			if (p.getOccupiedTerritories().contains(territoryTo))
 			{
-				temp.add(s);
+				System.out.println("Nice try you own this one");
+			} else if (!getAvailableNeighbors(p).contains(territoryTo))
+			{
+				System.out.println("can't attack");
+			} else
+			{
+				valid = true;
+			}
+		}
+		Territory t = TerritoryMap.get(territoryTo);
+		Set<String> available = new HashSet<String>();
+		for (String s : t.getAdjacentTerritories())
+		{
+			if (p.ownsTerritory(s))
+			{
+				available.add(s);
 			}
 		}
 		System.out.println("Choose a territory to attack from:");
-		System.out.println(temp);
-		String territoryFrom = br.readLine();
-		System.out.println("Choose number of armies to attack with. Opponent has " + TerritoryMap.get(territoryTo).getNumArmies());
+		System.out.println(available);
+		territoryFrom = br.readLine();
+
+		System.out.println(
+				"Choose number of armies to attack with. Opponent has " + TerritoryMap.get(territoryTo).getNumArmies());
 		System.out.println("You have: " + TerritoryMap.get(territoryFrom).getNumArmies());
-		int numArmies = Integer.parseInt(br.readLine());
+		valid = false;
+		numArmies = 0;
+		while (!valid)
+		{
+			numArmies = Integer.parseInt(br.readLine());
+			if (numArmies <= 0 || numArmies > TerritoryMap.get(territoryFrom).getNumArmies())
+			{
+				System.out.println("no");
+			} else
+			{
+				valid = true;
+			}
+		}
 		p.attackOther(TerritoryMap.get(territoryFrom), TerritoryMap.get(territoryTo), numArmies);
-		
+
 	}
-	//Lol o(n^2) what is this garbage code i just wrote
+
+	// Lol o(n^2) what is this garbage code i just wrote
 	public Set<String> getAvailableNeighbors(Player p)
 	{
 		Set<String> returnSet = new HashSet<String>();
 		Set<String> playerSet = p.getOccupiedTerritories();
-		for(String s:playerSet)
+		for (String s : playerSet)
 		{
 			Territory t = TerritoryMap.get(s);
 			Set<String> neighbors = t.getAdjacentTerritories();
-			for(String k : neighbors)
+			for (String k : neighbors)
 			{
-				if(!playerSet.contains(k))
+				if (!playerSet.contains(k))
 				{
 					returnSet.add(k);
 				}
@@ -92,91 +125,90 @@ public class Main
 		}
 		return returnSet;
 	}
-	
+
 	/*
 	 * Initialize the players and the TerritoryMap
 	 */
 	private void init()
 	{
 		TerritoryMap.init();
-		
+
 		// For cmd line inputs, will be replaced by GUI inputs
 		br = new BufferedReader(new InputStreamReader(System.in));
 
 		getNumPlayers();
-		players = new Player[numPlayers]; 
+		players = new Player[numPlayers];
 
 		// initialize players manually for testing purposes
 		players[0] = new Player("George", "Germany");
 		players[1] = new Player("Richard Test", "China");
 
-		//for(int i = 0; i < numPlayers; i++)
-		//{
-		//	String name = "Derp" + i;
-		//	String territoryID = "null";
-		//	
-		//	//TODO: Change these displays to GUI later and pass in values to variables
-		//	System.out.println("Player " + (i + 1) + ": Enter name");
-		//	System.out.println("Player " + (i + 1) + ": Choose Territory");
-		//	
-		//	players[i] = new Player(name, territoryID);
-		//}
+		// for(int i = 0; i < numPlayers; i++)
+		// {
+		// String name = "Derp" + i;
+		// String territoryID = "null";
+		//
+		// //TODO: Change these displays to GUI later and pass in values to
+		// variables
+		// System.out.println("Player " + (i + 1) + ": Enter name");
+		// System.out.println("Player " + (i + 1) + ": Choose Territory");
+		//
+		// players[i] = new Player(name, territoryID);
+		// }
 	}
 
 	private void deployReinforcements(Player p) throws IOException
 	{
-		
-			System.out.println("Phase 1: Deploy Reinforcements");
-			p.calculateReinforcements();
-			while(p.getNumReinforcementsAvailable() > 0)
+
+		System.out.println("Phase 1: Deploy Reinforcements");
+		p.calculateReinforcements();
+		while (p.getNumReinforcementsAvailable() > 0)
+		{
+			System.out.println("Total reinforcements for " + p.getName() + ": " + p.getNumReinforcementsAvailable());
+			System.out.print("Select territory to deploy to: ");
+			String territory = br.readLine();
+
+			if (!p.ownsTerritory(territory))
 			{
-				System.out.println("Total reinforcements for " + p.getName() + ": " + p.getNumReinforcementsAvailable());
-				System.out.print("Select territory to deploy to: ");
-				String territory = br.readLine();
-					 
-				if(!p.ownsTerritory(territory))
-				{
-					System.err.println("You do not own this territory. Try again");
-					continue;
-				}
-				
-				// Get the number of armies to deploy. Can also enter "all"
-				// If nothing entered, then assume 1 army
-				System.out.println(territory + " has " + TerritoryMap.get(territory).getNumArmies() + " armies on it");
-				System.out.print("How many armies to deploy? (default 1): ");
-				String numArmiesStr = br.readLine();
-				int numArmies = 1;
-				try
-				{
-					if(numArmiesStr.equals(""))
-						numArmies = 1;
-					else if(numArmiesStr.equals("all"))
-						numArmies = p.getNumReinforcementsAvailable();
-					else
-						numArmies =  Integer.parseInt(numArmiesStr);
-				}
-				catch(NumberFormatException e) // In case user enters non-numerical value
-				{
-					System.err.println("Bad number of armies to deploy, try again");
-					continue;
-				}
-				
-				// Check if numArmies is valid
-				if(numArmies < 0 || numArmies > p.getNumReinforcementsAvailable())
-				{
-					System.err.println("You do not have such number of armies, try again");
-				}
-				else if(numArmies == 0)
-				{
-					System.out.println("Cancelling deploy to " + territory);
-				}
-				else
-				{
-					System.out.println("Deploying " + numArmies + " to " + territory);
-					p.deployReinforcements(territory, numArmies);
-				}
-				System.out.println();
+				System.err.println("You do not own this territory. Try again");
+				continue;
 			}
+
+			// Get the number of armies to deploy. Can also enter "all"
+			// If nothing entered, then assume 1 army
+			System.out.println(territory + " has " + TerritoryMap.get(territory).getNumArmies() + " armies on it");
+			System.out.print("How many armies to deploy? (default 1): ");
+			String numArmiesStr = br.readLine();
+			int numArmies = 1;
+			try
+			{
+				if (numArmiesStr.equals(""))
+					numArmies = 1;
+				else if (numArmiesStr.equals("all"))
+					numArmies = p.getNumReinforcementsAvailable();
+				else
+					numArmies = Integer.parseInt(numArmiesStr);
+			} catch (NumberFormatException e) // In case user enters
+												// non-numerical value
+			{
+				System.err.println("Bad number of armies to deploy, try again");
+				continue;
+			}
+
+			// Check if numArmies is valid
+			if (numArmies < 0 || numArmies > p.getNumReinforcementsAvailable())
+			{
+				System.err.println("You do not have such number of armies, try again");
+			} else if (numArmies == 0)
+			{
+				System.out.println("Cancelling deploy to " + territory);
+			} else
+			{
+				System.out.println("Deploying " + numArmies + " to " + territory);
+				p.deployReinforcements(territory, numArmies);
+			}
+			System.out.println();
+		}
 	}
 
 	private void getNumPlayers()
