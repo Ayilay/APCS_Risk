0b2d3d6c2f7ef5b02854ba8ec3a7e03ff7199e82
Rmao99 <richard.mao.123@gmail.com>
2016-05-18 16:40:22 -0700

    Used CardLayout to switch between two pannels

--- src/userInterface/GUIManager.java
+++ src/userInterface/GUIManager.java
@@ -6,15 +6,21 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.Toolkit;
+import java.awt.CardLayout;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 import battle.BattleResults;
 import card.Card;
@@ -32,11 +38,20 @@ public class GUIManager implements UserInterface
 	private JPanel gameStateArea;
 	private JPanel footerArea;
 
+	private JPanel deck;
+	private JPanel initPane;
+	
+
+	private JButton cards;
+	private JButton back;
+	
+	private CardLayout cardDisplay;
+
 	private BufferedReader br;
 
 	private final int WIDTH = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 	private final int HEIGHT = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	
+
 	public GUIManager()
 	{
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -54,23 +69,30 @@ public class GUIManager implements UserInterface
 		gameStateArea = new JPanel();
 		footerArea = new JPanel();
 
+		deck = new JPanel();
+		initPane = new JPanel();
+
+		cards = new JButton("Display cards");
+		back = new JButton("Back");
 		
+		cardDisplay = new CardLayout();
+
 		GridBagConstraints constr = new GridBagConstraints();
-		
+
 		System.out.println(window.getWidth());
 		System.out.println(window.getHeight());
-		
+
 		//mainPane.setPreferredSize(new Dimension(1200, 700));
 		mainPane.setBackground(Color.BLACK);
-		
+
 		ImageIcon img = new ImageIcon("Risk_Final_Map.png");		
-		
+
 		window.add(mainPane);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //terminates the program on close
 		window.setVisible(true);
-//TODO: Setting the window to visible allows me to access the dimensions of the mainPane,
-//TODO: but I can not add anything to the window after this. Make sure setVisible is at the END
-		
+		//TODO: Setting the window to visible allows me to access the dimensions of the mainPane,
+		//TODO: but I can not add anything to the window after this. Make sure setVisible is at the END
+
 		//int width = mainPane.getWidth() - 100;
 		//int height = mainPane.getHeight() - 20;
 		//System.out.println(width);
@@ -81,7 +103,7 @@ public class GUIManager implements UserInterface
 		constr.weightx = 0;
 		constr.weighty = 0;
 
-		
+
 		// Player Name area
 		constr.gridx = 0;
 		constr.gridy = 0;
@@ -92,7 +114,7 @@ public class GUIManager implements UserInterface
 		playerNameArea.setVisible(true);
 		mainPane.add(playerNameArea, constr);
 
- 		// Message Area
+		// Message Area
 		constr.gridx = 1;
 		constr.gridy = 0;
 		constr.fill = GridBagConstraints.NONE;
@@ -101,7 +123,7 @@ public class GUIManager implements UserInterface
 		messageArea.setPreferredSize(new Dimension(1000,(600-512)));
 		messageArea.setVisible(true);
 		mainPane.add(messageArea, constr);
-		
+
 
 		// Game State area
 		constr.gridx = 0;
@@ -116,8 +138,32 @@ public class GUIManager implements UserInterface
 		constr.gridy = 3;
 		constr.gridheight = 2;
 		constr.fill = GridBagConstraints.VERTICAL;
+		playerStatsArea.setLayout(cardDisplay);
 		playerStatsArea.setBackground(Color.magenta);
 		playerStatsArea.setPreferredSize(new Dimension(200,100));
+
+		initPane.add(cards);
+		initPane.setBackground(Color.GREEN);
+		deck.add(back);
+		deck.setBackground(Color.MAGENTA);
+		
+		playerStatsArea.add(initPane, "1");
+		playerStatsArea.add(deck, "2");
+		
+		cardDisplay.show(playerStatsArea, "1");
+		
+		cards.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				cardDisplay.show(playerStatsArea, "2");
+			}
+		});
+		
+		back.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){
+				cardDisplay.show(playerStatsArea, "1");
+			}
+		});
+
 		playerStatsArea.setVisible(true);
 		mainPane.add(playerStatsArea, constr);
 
@@ -131,7 +177,7 @@ public class GUIManager implements UserInterface
 		mapArea.setPreferredSize(new Dimension(1000,512));
 		mapArea.setVisible(true);
 		mainPane.add(mapArea, constr);
-//1200
+
 		// Footer
 		constr.gridx = 1;
 		constr.gridy = 4;
@@ -140,9 +186,9 @@ public class GUIManager implements UserInterface
 		footerArea.setPreferredSize(new Dimension(100,10));
 		footerArea.setVisible(true);
 		mainPane.add(footerArea, constr);
-		
-	
-		
+
+
+
 		//Allows termination of program when closed
 		//window.addWindowListener(new java.awt.event.WindowAdapter()
 		//{
@@ -151,10 +197,10 @@ public class GUIManager implements UserInterface
 		//		System.exit(0);
 		//	}
 		//});
-		
+
 	}
-	
-	
+
+
 	////////////////////////////////////////////////////////////
 	//	Init Methods
 	////////////////////////////////////////////////////////////
@@ -191,6 +237,7 @@ public class GUIManager implements UserInterface
 	////////////////////////////////////////////////////////////
 	//	Use Card Methods
 	////////////////////////////////////////////////////////////
+
 
 	@Override
 	public Card selectCard(Player p)
