e69e2830f15f5f5bb3a38f16208a15bbc8b0abce
Relic56 <ethan.y.yao@gmail.com>
2016-05-22 10:42:58 -0700

    Fixed quebec button, need to commit so i can pull

--- src/buttons/QuebecButton.java
+++ src/buttons/QuebecButton.java
@@ -7,15 +7,17 @@ public class QuebecButton extends TerritoryButton
 	public QuebecButton(String s)
 	{
 		super(s);
-		this.setPreferredSize(new Dimension(70, 65));
-		shape.addPoint(0, 39);
-		shape.addPoint(30, 0);
-		shape.addPoint(60, 10);
-		shape.addPoint(70, 34);
+		this.setPreferredSize(new Dimension(81, 66));
+		this.setSize(new Dimension(81, 66));
+		this.setLocation(222, 69);
+		shape.addPoint(9, 39);
+		shape.addPoint(41, 0);
+		shape.addPoint(81, 33);
 		shape.addPoint(44, 42);
-		shape.addPoint(52, 58);
-		shape.addPoint(30, 65);
-		shape.addPoint(27, 52);
-		shape.addPoint(1, 55);
+		shape.addPoint(61, 55);
+		shape.addPoint(37, 66);
+		shape.addPoint(33, 54);
+		shape.addPoint(25, 59);
+		shape.addPoint(0, 54);
 	}
 }
