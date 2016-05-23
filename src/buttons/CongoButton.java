f3f6e61f1346df4b8e95c3d0d494613b36ab6ca6
Relic56 <ethan.y.yao@gmail.com>
2016-05-22 21:30:14 -0700

    Made some of the Africa buttons fit better

--- src/buttons/CongoButton.java
+++ src/buttons/CongoButton.java
@@ -7,20 +7,44 @@ public class CongoButton extends TerritoryButton
 	public CongoButton(String s)
 	{
 		super(s);
-		this.setPreferredSize(new Dimension(83, 83));
-		this.setSize(new Dimension(83, 83));
-		this.setLocation(491, 261);
-		shape.addPoint(0, 32);
+		this.setPreferredSize(new Dimension(87, 89));
+		this.setSize(new Dimension(87, 89));
+		this.setLocation(489, 260);
+		shape.addPoint(3, 33);
 		shape.addPoint(24, 33);
-		shape.addPoint(17, 18);
-		shape.addPoint(44, 0);
-		shape.addPoint(62, 25);
-		shape.addPoint(82, 27);
-		shape.addPoint(83, 36);
-		shape.addPoint(65, 57);
-		shape.addPoint(61, 83);
-		shape.addPoint(43, 79);
-		shape.addPoint(41, 65);
-		shape.addPoint(8, 61);
+		shape.addPoint(24, 30);
+		shape.addPoint(17, 19);
+		shape.addPoint(21, 13);
+		shape.addPoint(29, 12);
+		shape.addPoint(47, 0);
+		shape.addPoint(51, 10);
+		shape.addPoint(62, 19);
+		shape.addPoint(63, 25);
+		shape.addPoint(67, 24);
+		shape.addPoint(72, 27);
+		shape.addPoint(68, 27);
+		shape.addPoint(87, 35);
+		shape.addPoint(78, 45);
+		shape.addPoint(74, 45);
+		shape.addPoint(73, 54);
+		shape.addPoint(68, 58);
+		shape.addPoint(68, 62);
+		shape.addPoint(73, 71);
+		shape.addPoint(68, 71);
+		shape.addPoint(66, 77);
+		shape.addPoint(66, 84);
+		shape.addPoint(71, 87);
+		shape.addPoint(69, 89);
+		shape.addPoint(66, 89);
+		shape.addPoint(61, 84);
+		shape.addPoint(43, 80);
+		shape.addPoint(42, 66);
+		shape.addPoint(36, 66);
+		shape.addPoint(32, 71);
+		shape.addPoint(29, 71);
+		shape.addPoint(24, 62);
+		shape.addPoint(10, 62);
+		shape.addPoint(9, 56);
+		shape.addPoint(0, 50);
 	}
 }
--- src/buttons/MadagascarButton.java
+++ src/buttons/MadagascarButton.java
@@ -8,13 +8,18 @@ public class MadagascarButton extends TerritoryButton
 	public MadagascarButton(String s)
 	{
 		super(s);
-		this.setPreferredSize(new Dimension(26, 47));
-		this.setSize(new Dimension(26, 47));
-		this.setLocation(602, 346);
-		shape.addPoint(26, 0);
+		this.setPreferredSize(new Dimension(25, 47));
+		this.setSize(new Dimension(25, 47));
+		this.setLocation(602, 347);
+		shape.addPoint(21, 0);
+		shape.addPoint(25, 6);
+		shape.addPoint(25, 11);
 		shape.addPoint(12, 47);
-		shape.addPoint(0, 45);
-		shape.addPoint(6, 15);
+		shape.addPoint(2, 47);
+		shape.addPoint(0, 36);
+		shape.addPoint(4, 27);
+		shape.addPoint(4, 18);
+		shape.addPoint(8, 13);
+		shape.addPoint(10, 13);
 	}
-
 }
